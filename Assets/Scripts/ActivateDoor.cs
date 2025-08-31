17dcb8cb1271e235209eea3eb2876af1290ba7d0
bogger12 <23302164+bogger12@users.noreply.github.com>
2025-08-31 19:51:06 +1200

    Button opening door with animation

--- Assets/Scripts/ActivateDoor.cs
+++ Assets/Scripts/ActivateDoor.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 
-
+[RequireComponent(typeof(Animator))]
 public class ActivateDoor : Activateable
 {
-    public override void SetActive(bool active) {
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void SetActive(bool active)
+    {
         Debug.Log(transform.name + " set to" + active);
+        animator.SetBool("open", active);
     }
 }
