14029f71662ff6b880e3688bc236e06768f1a004
bogger12 <23302164+bogger12@users.noreply.github.com>
2025-09-02 01:54:48 +1200

    Implement splitscreen, adjust anchorOn3DPoint to match. Duplicate canvases and cameras

--- Assets/Scripts/AnchorOn3DPoint.cs
+++ Assets/Scripts/AnchorOn3DPoint.cs
@@ -7,12 +7,12 @@ public class AnchorOn3DPoint : MonoBehaviour
 
     public Transform anchor;
     private Vector3 anchorPos;
-    public Camera sceneCamera;
+    private Camera sceneCamera;
     public Vector2 offset2D;
     public bool lockToScreenBounds;
 
     private Vector2 defaultSize;
-    public float sizeMultiplier;
+    public float sizeMultiplier = 1;
 
     RectTransform rt;
     Canvas canvas;
@@ -22,18 +22,28 @@ public class AnchorOn3DPoint : MonoBehaviour
     {
         rt = GetComponent<RectTransform>();
         canvas = transform.GetComponentInParent<Canvas>();
+        sceneCamera = canvas.worldCamera;
         defaultSize = rt.sizeDelta;
     }
 
     void LateUpdate()
     {
         Vector3 anchorPoint = anchor != null ? anchor.position : anchorPos;
-        Vector2 screenPoint = sceneCamera.WorldToScreenPoint(anchorPoint);
+        Vector3 screenPoint = sceneCamera.WorldToScreenPoint(anchorPoint);
         if (lockToScreenBounds) screenPoint = new Vector2(Mathf.Clamp(screenPoint.x, 0, sceneCamera.pixelWidth), Mathf.Clamp(screenPoint.y, 0, sceneCamera.pixelHeight));
-        rt.position = screenPoint + offset2D * canvas.scaleFactor;
+
+        Vector2 uiPoint;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)canvas.transform,            // the root canvas RectTransform
+            screenPoint,                 // from step 2
+            sceneCamera,                       // the camera rendering this UI
+            out uiPoint
+        );
+
+        rt.anchoredPosition = uiPoint + offset2D * canvas.scaleFactor;
 
         rt.sizeDelta = defaultSize * sizeMultiplier;
-        Debug.Log("setting size mult to " + sizeMultiplier);
+        Debug.Log("Viewport Point " + screenPoint);
     }
 
     public void SetAnchorPos(Vector3 pos)
