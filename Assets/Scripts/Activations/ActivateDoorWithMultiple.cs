e8bdbe36d2a64b5e8343fa1066d9923f43f4e2ca
bogger12 <23302164+bogger12@users.noreply.github.com>
2025-09-05 17:53:47 +1200

    Color debug lines depending on active

--- Assets/Scripts/Activations/ActivateDoorWithMultiple.cs
+++ Assets/Scripts/Activations/ActivateDoorWithMultiple.cs
@@ -5,6 +5,7 @@ public class ActivateDoorWithMultiple : Activateable
 {
     private Animator animator;
 
+    public bool permanentlyOpens = false;
     public int activationsNeeded = 2;
     private int numActivated = 0;
 
@@ -18,7 +19,7 @@ public class ActivateDoorWithMultiple : Activateable
         numActivated = active ? numActivated + 1 : numActivated - 1;
         Debug.Log(transform.name + " set to" + active + "numActive: " + numActivated);
 
-        bool doorActive = numActivated == activationsNeeded;
+        bool doorActive = numActivated == activationsNeeded || (permanentlyOpens && active);
         animator.SetBool("open", doorActive);
         base.SetActive(doorActive);
         return active;
--- Assets/Scripts/Activations/RemoteActivate.cs
+++ Assets/Scripts/Activations/RemoteActivate.cs
@@ -7,15 +7,21 @@ public class RemoteActivate : MonoBehaviour
 
     public bool SetActive(bool active)
     {
+#if UNITY_EDITOR
+        lineColor = active ? Color.green : Color.red;
+#endif
         return activateable.SetActive(active);
     }
 
+#if UNITY_EDITOR
+    private Color lineColor = Color.red;
     void OnDrawGizmos()
     {
         if (activateable != null)
         {
-            Gizmos.color = Color.red;
+            Gizmos.color = lineColor;
             Gizmos.DrawLine(transform.position, activateable.GetComponentInChildren<Collider>().bounds.center);
         }
     }
+#endif
 }
