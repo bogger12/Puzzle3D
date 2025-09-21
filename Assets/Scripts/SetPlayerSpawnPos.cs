932dddb3677777782cb9404607d13b8879247fb2
Alex Schenkel <23302164+bogger12@users.noreply.github.com>
2025-09-21 22:07:04 +1200

    Add moving platform and spawning

--- Assets/Scripts/SetPlayerSpawnPos.cs
+++ Assets/Scripts/SetPlayerSpawnPos.cs
@@ -8,7 +8,7 @@ public class SetPlayerSpawnPos : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        spawnPoint.GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
@@ -20,13 +20,15 @@ public class SetPlayerSpawnPos : MonoBehaviour
     private void OnPlayerJoined(PlayerInput playerInput)
     {
         // Move the new player to the spawn position
-        playerInput.transform.position = spawnPoint.position;
-        playerInput.GetComponent<Rigidbody>().position = spawnPoint.position;
-        Debug.Log("Move player");
+        Respawner playerRespawner = playerInput.GetComponent<Respawner>();
+        playerRespawner.currentSpawnPoint = spawnPoint;
+        playerRespawner.Respawn();
+        Debug.Log("Spawn player at " + spawnPoint.gameObject.name);
     }
 
     void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(spawnPoint.position, Vector3.one);
+        Debug.DrawLine(transform.position, spawnPoint.position, Color.cyan);
     }
 }
