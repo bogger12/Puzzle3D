64f708d3b78edae66aed722455a4401fea7c277f
Alex Schenkel <23302164+bogger12@users.noreply.github.com>
2025-09-22 00:51:15 +1200

    Add stone sound

---
+++ Assets/Scripts/Sound/PlaySoundOnActivate.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnActivate : MonoBehaviour
+{
+
+    public AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlaySound()
+    {
+        int index = Random.Range(0, clips.Length - 1);
+        Debug.Log("Playing sound " + index);
+        audioSource.resource = clips[index];
+        audioSource.Play();
+    }
+    
+    private void OnCollisionEnter(Collision other) {
+        PlaySound();
+    }
+
+}
