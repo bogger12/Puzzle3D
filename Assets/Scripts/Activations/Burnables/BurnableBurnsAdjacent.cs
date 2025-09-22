fc51fd0484bf83a9ea24a828d2c4ed645dd80f4f
Patrick Sheng <sheng.patrick@gmail.com>
2025-09-22 17:14:31 +1200

    feat: final changes

--- Assets/Scripts/Activations/Burnables/BurnableBurnsAdjacent.cs
+++ Assets/Scripts/Activations/Burnables/BurnableBurnsAdjacent.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
+// using UnityEditor.PackageManager;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
@@ -7,64 +7,66 @@ public abstract class BurnableBurnsAdjacent : Burnable
 {
 
 
-    protected Collider nearbyBurnCollider;
-    public float delayBetweenNearbyBurns = 0;
-    public bool useDelayWhenAlreadyBurning = false;
+  protected Collider nearbyBurnCollider;
+  public float delayBetweenNearbyBurns = 0;
+  public bool useDelayWhenAlreadyBurning = false;
 
 
-    private List<Burnable> burnablesInsideBurnVolume = new List<Burnable>();
-    public virtual void Start()
-    {
-        nearbyBurnCollider = GetComponent<Collider>();
-    }
-    public override void StartBurn()
-    {
-        base.StartBurn();
-        // Start burning nearby objects
-        
-        if (burnablesInsideBurnVolume.Count>0) foreach (Burnable b in burnablesInsideBurnVolume)
+  private List<Burnable> burnablesInsideBurnVolume = new List<Burnable>();
+  public virtual void Start()
+  {
+    nearbyBurnCollider = GetComponent<Collider>();
+  }
+  public override void StartBurn()
+  {
+    base.StartBurn();
+    // Start burning nearby objects
+
+    if (burnablesInsideBurnVolume.Count > 0) foreach (Burnable b in burnablesInsideBurnVolume)
+      {
+        if (!b.burning)
         {
-            if (!b.burning)
-            {
-                StartCoroutine(StartBurnAfter(delayBetweenNearbyBurns, b));
-                // Debug.Log(transform.parent.name + "burns " + b.transform.parent.name + " after start burning");
-            }
+          StartCoroutine(StartBurnAfter(delayBetweenNearbyBurns, b));
+          // Debug.Log(transform.parent.name + "burns " + b.transform.parent.name + " after start burning");
         }
-    }
+      }
+  }
 
-    public void OnTriggerEnter(Collider other)
+  public void OnTriggerEnter(Collider other)
+  {
+    // if (other.isTrigger) return;
+    Burnable burnable = other.GetComponentInChildren<Burnable>();
+    if (burning && burnable != null)
     {
-        // if (other.isTrigger) return;
-        Burnable burnable = other.GetComponentInChildren<Burnable>();
-        if (burning && burnable!=null)
-        {
-            float delay = useDelayWhenAlreadyBurning ? delayBetweenNearbyBurns : 0;
-            if (!burnable.burning)
-            {
-                StartCoroutine(StartBurnAfter(delay, burnable));
-                // Debug.Log(transform.parent.name + "burns " + burnable.transform.parent.name);
-            }
-        } else if (burnable!=null) {
-            burnablesInsideBurnVolume.Add(burnable);
-        }
+      float delay = useDelayWhenAlreadyBurning ? delayBetweenNearbyBurns : 0;
+      if (!burnable.burning)
+      {
+        StartCoroutine(StartBurnAfter(delay, burnable));
+        // Debug.Log(transform.parent.name + "burns " + burnable.transform.parent.name);
+      }
     }
+    else if (burnable != null)
+    {
+      burnablesInsideBurnVolume.Add(burnable);
+    }
+  }
 
-    public void OnTriggerExit(Collider other)
+  public void OnTriggerExit(Collider other)
+  {
+    // if (other.isTrigger) return;
+    Burnable burnable = other.GetComponentInChildren<Burnable>();
+    if (burnable != null)
     {
-        // if (other.isTrigger) return;
-        Burnable burnable = other.GetComponentInChildren<Burnable>();
-        if (burnable!=null)
-        {
-            burnablesInsideBurnVolume.Remove(burnable);
-        }
+      burnablesInsideBurnVolume.Remove(burnable);
     }
+  }
 
-    private IEnumerator<WaitForSeconds> StartBurnAfter(float duration, Burnable b)
-    {
-        // Eventual code to execute right as the function is called
-        yield return new WaitForSeconds(duration);
-        // The code from here will be executed after **duration** seconds
-        if (b == null) yield break;
-        b.StartBurn();
-    }
+  private IEnumerator<WaitForSeconds> StartBurnAfter(float duration, Burnable b)
+  {
+    // Eventual code to execute right as the function is called
+    yield return new WaitForSeconds(duration);
+    // The code from here will be executed after **duration** seconds
+    if (b == null) yield break;
+    b.StartBurn();
+  }
 }
--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,21 +5,21 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
 
-    public KeyCode keyToLoad = KeyCode.Return;
-    public SceneAsset sceneToLoad;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+  public KeyCode keyToLoad = KeyCode.Return;
+  public string sceneToLoad;
+  // Start is called once before the first execution of Update after the MonoBehaviour is created
+  void Start()
+  {
 
-    // Update is called once per frame
-    void Update()
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKeyDown(keyToLoad))
     {
-        if (Input.GetKeyDown(keyToLoad))
-        {
-            Debug.Log("Loading Scene " + sceneToLoad.name);
-            SceneManager.LoadScene(sceneToLoad.name, LoadSceneMode.Single);
-        }
+      Debug.Log("Loading Scene " + sceneToLoad);
+      SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
     }
+  }
 }
